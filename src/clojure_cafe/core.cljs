ba6f34d75144b73c815d14526006ae3c5f189c70
Ticean Bennett <ticean@gmail.com>
2015-05-30 14:18:53 -0700

    Initial commit

---
+++ src/clojure_cafe/core.cljs
@@ -0,0 +1,24 @@
+(ns ^:figwheel-always clojure-cafe.core
+  (:require
+    [om.core :as om :include-macros true]
+    [om.dom :as dom :include-macros true]))
+
+
+(enable-console-print!)
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [data owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text data)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
